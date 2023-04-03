db7fa06229c8c7738e16fb6254d320c29ba060c7
Kate Lanman <katelanman@Kates-MacBook-Pro.local>
2023-04-03 17:04:29 -0400

    linking done

--- js/main.js
+++ js/main.js
@@ -14,8 +14,12 @@ function getFeaturesInView(map) {
   let features = [];
   map.eachLayer( function(layer) {
     try {
-      if(map.getBounds().contains(layer.getLatLng())) {
-        features.push(layer.options.id);
+      if (map.getBounds().contains(layer.getLatLng())) {    
+      	if ( (!(d3.select("#bf-legend-filter").classed("off")) && layer.options.id.startsWith("bf")) || 
+      		 ( !(d3.select("#ufo-legend-filter").classed("off")) && layer.options.id.startsWith("ufo"))) {
+
+      		features.push(layer.options.id);
+      	}
       }
     } catch (error) {};
   });
@@ -25,18 +29,18 @@ function getFeaturesInView(map) {
 
 
 // filter function used for points on the map
-function filter(event) {
+function filter(event, object) {
 	// checks if current selection has class of bigfoot sighting
-	if (d3.select(this).classed("bf-legend")) {
+	if (d3.select(object).classed("bf-legend")) {
 		// checks if the selection doesn't have the class of off
-		if (!(d3.select(this).classed("off"))) {
+		if (!(d3.select(object).classed("off"))) {
 			// selects all elements with class of bigfoot active
 			// changes the class to bf-inactive, and adds the off class
 			d3.selectAll(".bf-active").classed("bf-inactive", true).classed("bf-active", false);
-			d3.select(this).classed("off", true);
+			d3.select(object).classed("off", true);
 			d3.selectAll(".bar-bf-active").classed("bar-bf-inactive", true).classed("bar-bf-active", false);
-			d3.select(this).classed("off", true);
-			d3.select(this).style("stroke", "white").style("fill", "white");
+			d3.select(object).classed("off", true);
+			d3.select(object).style("stroke", "white").style("fill", "white");
 		}
 
 		// class of off
@@ -44,32 +48,32 @@ function filter(event) {
 			// selects all inactive class, makes them have the active class
 			// takes away the off class
 			d3.selectAll(".bf-inactive").classed("bf-active", true).classed("bf-inactive", false);
-			d3.select(this).classed("off", false)
+			d3.select(object).classed("off", false);
 			d3.selectAll(".bar-bf-inactive").classed("bar-bf-active", true).classed("bar-bf-inactive", false);
-			d3.select(this).classed("off", false);
-			d3.select(this).style("stroke", "orange").style("fill", "orange");
+			d3.select(object).classed("off", false);
+			d3.select(object).style("stroke", "orange").style("fill", "orange");
 		}
 	}
 
-	else {
+	else if (d3.select(object).classed("ufo-legend")) {
 		// repeat the same for UFOs
 		// no off class
-		if (!d3.select(this).classed("off")) {
+		if (!d3.select(object).classed("off")) {
 			// change from active to inactive & add off class
 			d3.selectAll(".ufo-active").classed("ufo-inactive", true).classed("ufo-active", false);
-			d3.select(this).classed("off", true)
+			d3.select(object).classed("off", true);
 			d3.selectAll(".bar-ufo-active").classed("bar-ufo-inactive", true).classed("bar-ufo-active", false);
-			d3.select(this).classed("off", true);
-			d3.select(this).style("stroke", "white").style("fill", "white");
+			d3.select(object).classed("off", true);
+			d3.select(object).style("stroke", "white").style("fill", "white");
 		}
 
 		else {
 			// inactive to active, remove off class
 			d3.selectAll(".ufo-inactive").classed("ufo-active", true).classed("ufo-inactive", false);
-			d3.select(this).classed("off", false)
+			d3.select(object).classed("off", false);
 			d3.selectAll(".bar-ufo-inactive").classed("bar-ufo-active", true).classed("bar-ufo-inactive", false);
-			d3.select(this).classed("off", false);
-			d3.select(this).style("stroke", "steelblue").style("fill", "steelblue");
+			d3.select(object).classed("off", false);
+			d3.select(object).style("stroke", "steelblue").style("fill", "steelblue");
 		}
 	}
 };
@@ -100,7 +104,8 @@ Promise.all([d3.csv("data/bigfoot.csv"),
 
 	// leaflet map
 	L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-		attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+		attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, \
+									GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
 	}).addTo(mymap);
 
 	// loop through the bigfoot file
@@ -108,9 +113,11 @@ Promise.all([d3.csv("data/bigfoot.csv"),
 
 		// function to be called when a point on the map is clicked
 		function pointClicked() {
-			let text0 = "<strong>Summary of Bigfoot Sighting: </strong>"
+			let text0 = "<strong>Summary of Bigfoot Sighting: </strong>";
+
 			// text of bigfoot sighting
 			let text1 = files[0][key].observed;
+
 			// add the text to the scroll box
 			document.getElementById("scroll-box").innerHTML = text0 + "<br>" + text1;
 		}
@@ -193,11 +200,17 @@ Promise.all([d3.csv("data/bigfoot.csv"),
 			.append("circle")
 				.attr("class", (d) => {if (d.label == "Bigfoot Sighting") {return "bf-legend"}
 																else {return "ufo-legend"}})
+				.attr("id", (d) => {if (d.label == "Bigfoot Sighting") {return "bf-legend-filter"}
+																else {return "ufo-legend-filter"}})
 				.attr("r", 7)
 				.attr("cx", 30)
 				.attr("cy", (d, i) => {return (i + 1) / 2 * 60 + 40})
 				.style("fill", (d) => {return d.color})
-				.on("click", filter);
+				.on("click", function (e) {
+						filter(e, this);
+						getFeaturesInView(mymap);
+	    			plotBar();
+					});
 
 	// use categories array to add text labels to the legend
 	svg.selectAll("labels").data(categories).enter()
@@ -208,7 +221,13 @@ Promise.all([d3.csv("data/bigfoot.csv"),
 
 	getFeaturesInView(mymap);
 
+	// filter bar chart based on points in view
 	mymap.on('zoomend', function() {
+    getFeaturesInView(mymap);
+    plotBar();
+	});
+
+	mymap.on('moveend', function() {
     getFeaturesInView(mymap);
     plotBar();
 	});
@@ -217,28 +236,38 @@ Promise.all([d3.csv("data/bigfoot.csv"),
 });
 
 
-
 // set the dimensions and margins of the graph
-const margin = {top: 10, right: 30, bottom: 50, left: 50},
-    width = 740 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-const svg2 = d3.select("#vis1")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",`translate(${margin.left},${margin.top})`);
-
-// define x and y axis labels
-const xAxisLabel = "Month";
-const yAxisLabel = "Number of Sightings";
+const MARGIN = {top: 10, right: 30, bottom: 50, left: 50},
+    WIDTH = 740 - MARGIN.left - MARGIN.right,
+    HEIGHT = 400 - MARGIN.top - MARGIN.bottom;
 
 function plotBar() {
 	Promise.all([d3.csv("data/bigfoot.csv"),
 			 d3.csv("data/ufos.csv"), 
 			 ]).then((files) => {
+
+	d3.select("#vis1").selectAll("*").remove();
+
+	// add title
+  d3.select("#vis1").append("svg")
+  	.attr("width", WIDTH)
+  	.attr("height", "50px")
+	  	.append("text")
+		  	.attr("x", MARGIN.left)
+		  	.attr("y", "25px")
+		  	.style("font-size", "20px")
+		  	.style("font-weight", "bold")
+		  	.style("margin", "10px")
+		  	.style("text-decoration", "underline")
+		  	.text("Number of Sightings by Month");
+
+	// append the svg object to the body of the page
+	let svg2 = d3.select("#vis1")
+			  .append("svg")
+			    .attr("width", WIDTH + MARGIN.left + MARGIN.right)
+			    .attr("height", HEIGHT + MARGIN.top + MARGIN.bottom)
+			  .append("g")
+			    .attr("transform",`translate(${MARGIN.left},${MARGIN.top})`);
 
 	// gather month counts for bf and ufo
 	let monthsData = [{month: 'January', data: {bf: {count: 0, label: 'bigfoot'}, ufo: {count: 0, label: 'ufo'}}}, {month: 'February', data: {bf: {count: 0, label: 'bigfoot'}, ufo: {count: 0, label: 'ufo'}}}, 
@@ -266,23 +295,32 @@ function plotBar() {
 
 	const X = d3.scaleBand()
       .domain(MONTHS)
-      .range([0, width])
+      .range([0, WIDTH])
       .padding([0.2]);
 
   svg2.append("g")
-    .attr("transform", `translate(0, ${height})`)
+    .attr("transform", `translate(0, ${HEIGHT})`)
     .call(d3.axisBottom(X).tickSize(0));
 
   // Add X-axis label
   svg2.append("text")
-    .attr("transform", `translate(${width / 2}, ${height + 30})`)
+    .attr("transform", `translate(${WIDTH / 2}, ${HEIGHT + 30})`)
     .style("text-anchor", "middle")
     .text("Month");
 
+  // Add Y-axis label
+  svg2.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - MARGIN.left)
+    .attr("x", 0 - (HEIGHT / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Number of Sightings");
+
   // Add Y axis
   const Y = d3.scaleLinear()
-    .domain([0, 120])
-    .range([ height, 0 ]);
+    .domain([0, 130])
+    .range([ HEIGHT, 0 ]);
 
   svg2.append("g")
     .call(d3.axisLeft(Y));
@@ -297,23 +335,69 @@ function plotBar() {
 		.data(monthsData)
 		.enter()
 		.append("rect")
-			.attr("class", "bar-bf-active")
-			.attr("x", (d) => {return X(d.month) + (xSubgroup.bandwidth() * 4) + 0.5})
+			.attr("class", "bar bar-bf-active")
+			.attr("x", (d) => {return X(d.month) + (xSubgroup.bandwidth() * 6) + 3})
 			.attr("y", (d) => {return Y(d.data.bf.count)})
-			.attr("width", xSubgroup.bandwidth() * 4)
-			.attr("height", (d) => {return height - Y(d.data.bf.count)})
+			.attr("width", xSubgroup.bandwidth() * 6)
+			.attr("height", (d) => {return HEIGHT - Y(d.data.bf.count)})
 			.attr("fill", "orange");
 
 	svg2.selectAll("bars")
 		.data(monthsData)
 		.enter()
 		.append("rect")
-			.attr("class", "bar-ufo-active")
+			.attr("class", "bar bar-ufo-active")
 			.attr("x", (d) => {return X(d.month)})
 			.attr("y", (d) => {return Y(d.data.ufo.count)})
-			.attr("width", xSubgroup.bandwidth() * 4)
-			.attr("height", (d) => {return height - Y(d.data.ufo.count)})
+			.attr("width", xSubgroup.bandwidth() * 6)
+			.attr("height", (d) => {return HEIGHT - Y(d.data.ufo.count)})
 			.attr("fill", "steelblue");
+
+  // Tooltip
+
+  // To add a tooltip, we will need a blank div that we 
+  //  fill in with the appropriate text. Be use to note the
+  //  styling we set here and in the .css
+  const TOOLTIP = d3.select("#vis1")
+                      .append("div")
+                        .attr("class", "tooltip")
+                        .style("opacity", 0); 
+
+  // Define event handler functions for tooltips
+  function handleMouseover(event, d) {
+    if (d3.select(this).classed("bar-bf-active")) {
+  		// on mouseover, make opaque 
+  		TOOLTIP.style("opacity", 1); 
+  		TOOLTIP.html("Kind: Bigfoot" + "<br>Value: " + d.data.bf.count);
+		}
+
+		else if (d3.select(this).classed("bar-ufo-active")) {
+			// on mouseover, make opaque 
+  		TOOLTIP.style("opacity", 1); 
+  		TOOLTIP.html("Kind: UFO" + "<br>Value: " + d.data.ufo.count);
+
+		}
+
+  }
+
+  function handleMousemove(event, d) {
+    // position the tooltip and fill in information 
+    TOOLTIP.style("left", (event.pageX ) + "px") //add offset
+                                                        // from mouse
+            .style("top", (event.pageY +5) + "px"); 
+  }
+
+  function handleMouseleave(event, d) {
+    // on mouseleave, make transparant again 
+    TOOLTIP.style("opacity", 0); 
+  } 
+
+
+  // Add event listeners
+  svg2.selectAll(".bar")
+        .on("mouseover", handleMouseover) //add event listeners
+        .on("mousemove", handleMousemove)
+        .on("mouseleave", handleMouseleave);  
 
 });
 
@@ -357,14 +441,7 @@ plotBar();
 //   svg2.append("g")
 //     .call(d3.axisLeft(y));
 
-//   // Add Y-axis label
-//   svg2.append("text")
-//     .attr("transform", "rotate(-90)")
-//     .attr("y", 0 - margin.left)
-//     .attr("x", 0 - (height / 2))
-//     .attr("dy", "1em")
-//     .style("text-anchor", "middle")
-//     .text(yAxisLabel);
+
 
 //   // Another scale for subgroup position?
 //   const xSubgroup = d3.scaleBand()
@@ -404,49 +481,7 @@ plotBar();
 //       .attr("height", d => height - y(d.value))
 //       .attr("fill", d => color(d.key))
 // 	  .attr("class", d => clas(d.key))
-// 	  .attr("id", "bar");	
-	  
-	  
-
-
-
-
-// 	  // Tooltip
-
-//      // To add a tooltip, we will need a blank div that we 
-//     //  fill in with the appropriate text. Be use to note the
-//     //  styling we set here and in the .css
-//     const TOOLTIP = d3.select("#vis1")
-//                         .append("div")
-//                           .attr("class", "tooltip")
-//                           .style("opacity", 0); 
-
-//     // Define event handler functions for tooltips
-//     function handleMouseover(event, d) {
-//       if (d3.select(this).classed("bar-bf-active") || d3.select(this).classed("bar-ufo-active") ) {
-//     		// on mouseover, make opaque 
-//     		TOOLTIP.style("opacity", 1); 
-//     		TOOLTIP.html("Kind: " + i_d(d.key) + "<br>Value: " + d.value);
-//   		}
-//     }
-//     function handleMousemove(event, d) {
-//       // position the tooltip and fill in information 
-//       TOOLTIP.style("left", (event.pageX ) + "px") //add offset
-//                                                           // from mouse
-//               .style("top", (event.pageY +5) + "px"); 
-//     }
-
-//     function handleMouseleave(event, d) {
-//       // on mouseleave, make transparant again 
-//       TOOLTIP.style("opacity", 0); 
-//     } 
-
-
-//     // Add event listeners
-//     svg2.selectAll("#bar")
-//           .on("mouseover", handleMouseover) //add event listeners
-//           .on("mousemove", handleMousemove)
-//           .on("mouseleave", handleMouseleave);    
+// 	  .attr("id", "bar");	  
 
 // });
 
